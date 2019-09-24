210fe71642f44392bd019a217b83645edb6b365e
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-09-23 23:51:34 -0300

    Mais Testes

---
+++ testeparalel4.sh
@@ -0,0 +1,6 @@
+./bin/paralel 1000 3 time 4
+./bin/paralel 10000 3 time 4
+./bin/paralel 100000 3 time 4
+./bin/paralel 1000000 3 time 4
+./bin/paralel 10000000 3 time 4
+./bin/paralel 100000000 3 time 4
